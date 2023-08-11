b3f9c77460b184eb25805d805015f97127aaa79b
Andrew Marabante <andrewmarabante@gmail.com>
2023-08-11 16:04:35 -0700

    Successfully console.logged my first API request!!

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-alert("working");
+document.getElementById("check").addEventListener("click",(function(){fetch("https://api.weatherapi.com/v1/current.json?key=6ad84aae75f24b31a40222612231108&q=temecula",{mode:"cors"}).then((function(e){console.log(e.json())}))}));
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert('working')
+const check = document.getElementById('check');
+check.addEventListener('click', checkAPI);
+
+function checkAPI()
+{
+    fetch('https://api.weatherapi.com/v1/current.json?key=6ad84aae75f24b31a40222612231108&q=temecula',  {mode: 'cors'})
+    .then(function(response) {
+        console.log(response.json());
+      });
+}
