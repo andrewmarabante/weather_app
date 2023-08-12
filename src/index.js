ecb94f3eeb3e78e2714fdd0b4d33aa032de55c8c
Andrew Marabante <andrewmarabante@gmail.com>
2023-08-11 19:11:08 -0700

    figured out how to retrieve info from API

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ function checkAPI(e)
     e.preventDefault();
     fetch(`https://api.weatherapi.com/v1/current.json?key=6ad84aae75f24b31a40222612231108&q=${location}`,  {mode: 'cors'})
     .then(function(response) {
-        console.log(response.json());
+        return response.json();
+      })
+      .then(function(response) {
+        console.log(response.current);
       });
 }
