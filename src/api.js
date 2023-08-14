1ac1bd1bcb1ef60cf17c03c65685b27dfdea554a
Andrew Marabante <andrewmarabante@gmail.com>
2023-08-14 16:47:26 -0700

    Created SetData to process the API information

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-export {changeWeatherData};
-import { weatherData, currentLocation } from ".";
+export {changeWeatherData, processData};
+import { weatherData, currentLocation, setData} from ".";
 //Returns weatherData object as json
 
 
@@ -7,11 +7,23 @@ async function changeWeatherData(e)
 {
   e.preventDefault();
   currentLocation = document.getElementById('formlocation').value;
-    fetch(`https://api.weatherapi.com/v1/current.json?key=6ad84aae75f24b31a40222612231108&q=temecula${currentLocation}`,  {mode: 'cors'})
+    fetch(`https://api.weatherapi.com/v1/current.json?key=6ad84aae75f24b31a40222612231108&q=${currentLocation}`,  {mode: 'cors'})
     .then(function(response) {
         return response.json();
       })
     .then(function(response) {
       weatherData = response.current;
+      setData;
     })
+}
+
+function processData()
+{
+  let fahrenheit = weatherData.temp_f
+  let celsius = weatherData.temp_c
+  let wind = weatherData.wind_mph
+  let humidity = weatherData.humidity
+ return{
+  fahrenheit, celsius, wind, humidity
+ }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import { changeWeatherData } from "./api";
-export {weatherData, currentLocation}
+import { changeWeatherData, processData } from "./api";
+export {weatherData, currentLocation, setData}
 let weatherData;
+let processedData;
 let currentLocation;
+
 const check = document.getElementById('check');
 check.addEventListener('click', changeWeatherData);
 const bla = document.getElementById('bla');
-bla.addEventListener('click', blabla)
+bla.addEventListener('click', setData)
 
 
-function blabla()
+function setData()
 {
     console.log(weatherData);
+    processedData = new processData(weatherData);
+    console.log(processedData);
 }
